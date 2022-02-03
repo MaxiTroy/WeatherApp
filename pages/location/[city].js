c8e6ed45ba4b01027c17cdcc5ea2b8c8aa92cf60
MaxiT <maxitroyt@gmail.com>
2022-02-02 23:18:40 -0300

    Add TodayWeather

--- pages/location/[city].js
+++ pages/location/[city].js
@@ -1,16 +1,49 @@
 import React from "react";
 import cities from "../../lib/city.list.json";
+import Head from "next/head";
+import TodaysWeather from "../../componets/TodaysWeather";
+import moment from "moment-timezone";
 
 export const getServerSideProps = async (context) => {
   const city = getCity(context.params.city);
-  console.log(city);
-  const slug = context.params.city;
-  console.log("context", context.params);
+
+  if (!city) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=${process.env.API_KEY}&units=metric&exclude=minutely`
+  );
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const hourlyWeather = getHourlyWeather(data.hourly, data.timezone);
+
   return {
     props: {
-      slug: slug,
+      city: city,
+      timezone: data.timezone,
+      currentWeather: data.current,
+      dailyWeather: data.daily,
+      hourlyWeather: hourlyWeather,
     },
   };
+};
+
+const getHourlyWeather = (houtlyData, timezone) => {
+  const endOfDay = moment().tz(timezone).endOf("day").valueOf();
+  const endTimeStamp = Math.floor(endOfDay / 1000);
+
+  const todaysData = houtlyData.filter((data) => data.dt < endTimeStamp);
+
+  return todaysData;
 };
 
 const getCity = (param) => {
@@ -23,11 +56,28 @@ const getCity = (param) => {
   if (city) return city;
   else return null;
 };
-const City = ({ slug }) => {
+const City = ({
+  hourlyWeather,
+  currentWeather,
+  dailyWeather,
+  city,
+  timezone,
+}) => {
   return (
     <div>
-      <h1>City Page</h1>
-      <h2>{slug}</h2>
+      <Head>
+        <title>{city.name}</title>
+      </Head>
+
+      <div className="page-wrapper">
+        <div className="container">
+          <TodaysWeather
+            city={city}
+            weather={dailyWeather[0]}
+            timezone={timezone}
+          />
+        </div>
+      </div>
     </div>
   );
 };
